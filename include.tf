a083557785eec05963506634130d4be78ed6152f
Graham Gilbert <graham.gilbert@airbnb.com>
2019-06-12 08:36:29 -0700

    Add in remote backend config

--- include.tf
+++ include.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "grahamgilbert"
+
+    workspaces {
+      name = "macdevops-2019-munkireport"
+    }
+  }
+}
